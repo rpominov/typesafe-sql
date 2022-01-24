d986281fbd0cc0f1989f7c77386a32ad2229f301
Roman <rpominov@gmail.com>
2022-01-24 19:15:51 +0300

    got to auth request

--- src/test.js
+++ src/test.js
@@ -1,11 +1,20 @@
 const net = require('net');
+
+let fixSize = (buffer, lastPos) => {
+    let size = lastPos + 4
+    let finalBuffer = Buffer.alloc(size)
+    finalBuffer.writeInt32BE(size)
+    buffer.copy(finalBuffer, 4)
+    return finalBuffer
+}
+
 const client = net.createConnection({ port: 5432, host: "localhost" }, () => {
     // 'connect' listener.
     console.log('connected to server!');
 
-    let buffer = Buffer.alloc(31);
-    let pos = 0
-    pos = buffer.writeInt32LE(0x00030000, pos)
+    let buffer = Buffer.alloc(128);
+    let pos = 0;
+    pos = buffer.writeInt32BE(0x00030000, pos)
     pos += buffer.write("user", pos)
     pos = buffer.writeUInt8(0, pos)
     pos += buffer.write("roman", pos)
@@ -16,14 +25,14 @@ const client = net.createConnection({ port: 5432, host: "localhost" }, () => {
     pos = buffer.writeUInt8(0, pos)
     pos = buffer.writeUInt8(0, pos)
 
-    console.log(pos)
-
-    client.write(buffer);
+    client.write(fixSize(buffer, pos));
 });
+
 client.on('data', (data) => {
     console.log(data.toString());
     client.end();
 });
+
 client.on('end', () => {
     console.log('disconnected from server');
 });
