7ebf7f955bdb41009f63859ae89c03ec78fa8ddf
Roman <rpominov@gmail.com>
2022-01-26 20:13:25 +0300

    better

--- packages/describe-query/index.js
+++ packages/describe-query/index.js
@@ -1,177 +1,170 @@
-const { Client } = require('pg')
-
-function prepareTypesInfo(namespaces, typeResponse) {
-  return new Map(typeResponse.rows.map(row => [row.oid, { ...row, namespace: namespaces.get(row.typnamespace) }]));
-}
-
-function prepareColumnsInfo(namespaces, classResponse, columnsResponse) {
-  const tables = new Map(classResponse.rows.map(row => {
-    const namespace = namespaces.get(row.relnamespace);
-    const key = [namespace.nspname, row.relname].join(',');
-    return [key, { ...row, namespace: namespace }]
-  }))
-
-  return new Map(columnsResponse.rows.map(row => {
-    const table = tables.get([row.table_schema, row.table_name].join(','))
-    const key = [table.oid, row.ordinal_position].join(',')
-    return [key, { ...row, table: table }]
-  }))
-}
-
-function prepareData(data, typesInfo, columnsInfo) {
-  return {
-    input: data.parameterDescription.dataTypeIDs.map(id => typesInfo.get(id)),
-    output: data.rowDescription == null ? null : data.rowDescription.fields.map(field => {
-      return {
-        name: field.name,
-        type: typesInfo.get(field.dataTypeID),
-        collumn: columnsInfo.get([field.tableID, field.columnID].join(','))
-      }
-    }),
-  };
-}
+const { Client } = require("pg");
 
 class DescribeClient {
-  constructor(pgClientOptions) {
-    this._pgClient = new Client(pgClientOptions)
-    this._connection = null
-    this._connected = false
-    this._terminated = false
-    this._connecting = false
-    this._currentRequestCb = null
-    this._rowDescription = null
-    this._parameterDescription = null
-    this._typesInfo = null;
-    this._columnsInfo = null;
+  constructor(options) {
+    this._options = options;
+    this._connection = null;
+    this._types = null;
+    this._columns = null;
+    this._listener = null;
   }
-  _handleEnd() {
-    if (this._currentRequestCb !== null) {
-      this._callCurrentRequestCb(new Error('Connection terminated'))
-    }
-    this._terminated = true
-  }
-  _handleError(err) {
-    if (this._currentRequestCb !== null) {
-      this._callCurrentRequestCb(err)
-    } else {
-      console.error("Unexpected error:", err)
-    }
-  }
-  _callCurrentRequestCb(err, data) {
-    const cb = this._currentRequestCb;
-    this._currentRequestCb = null;
-    cb(err, data)
-  }
-  _callCurrentRequestCbWithData() {
-    const data = { rowDescription: this._rowDescription, parameterDescription: this._parameterDescription }
-    this._rowDescription = null
-    this._parameterDescription = null
-    this._callCurrentRequestCb(null, data)
-  }
-  _handleRowDescription(msg) {
-    if (this._currentRequestCb === null) {
-      console.error("Unexpected RowDescription message:", msg)
-    } else {
-      this._rowDescription = msg
-    }
-  }
-  _handleParameterDescription(msg) {
-    if (this._currentRequestCb === null) {
-      console.error("Unexpected ParameterDescription message:", msg)
-    } else {
-      this._parameterDescription = msg
-    }
-  }
-  _handleReadyForQuery() {
-    if (this._currentRequestCb === null) {
-      console.error("Unexpected ReadyForQuery message")
-    } else {
-      this._callCurrentRequestCbWithData()
-    }
-  }
-  async connect() {
-    if (this._connected) {
-      return Promise.reject(new Error('Already connected'))
-    }
 
-    if (this._connecting) {
-      return Promise.reject(new Error('Already connecting'))
-    }
-
-    if (this._terminated) {
-      return Promise.reject(new Error('The client has been terminated'))
-    }
-
-    this._connecting = true;
-
-    await this._pgClient.connect()
+  async _connect() {
+    const pgClient = new Client(this._options);
+    await pgClient.connect();
 
     let namespaces = new Map(
-      (await this._pgClient.query("select * from pg_catalog.pg_namespace"))
-        .rows
-        .map(row => [row.oid, row])
-    )
-    this._typesInfo = prepareTypesInfo(namespaces, await this._pgClient.query("select * from pg_catalog.pg_type"))
-    this._columnsInfo = prepareColumnsInfo(
-      namespaces,
-      await this._pgClient.query("select * from pg_catalog.pg_class"),
-      await this._pgClient.query("select * from information_schema.columns")
-    )
+      (await pgClient.query("select * from pg_catalog.pg_namespace")).rows.map(
+        (row) => [row.oid, row]
+      )
+    );
+
+    this._types = new Map(
+      (await pgClient.query("select * from pg_catalog.pg_type")).rows.map(
+        (row) => [
+          row.oid,
+          { ...row, namespace: namespaces.get(row.typnamespace) },
+        ]
+      )
+    );
+
+    const tables = new Map(
+      (await pgClient.query("select * from pg_catalog.pg_class")).rows.map(
+        (row) => {
+          const namespace = namespaces.get(row.relnamespace);
+          const key = [namespace.nspname, row.relname].join(",");
+          return [key, { ...row, namespace: namespace }];
+        }
+      )
+    );
+
+    this._columns = new Map(
+      (
+        await pgClient.query("select * from information_schema.columns")
+      ).rows.map((row) => {
+        const table = tables.get([row.table_schema, row.table_name].join(","));
+        const key = [table.oid, row.ordinal_position].join(",");
+        return [key, { ...row, table: table }];
+      })
+    );
 
     // We want to steal the connection from the client and use it directly from now on
-    this._pgClient.connection.removeAllListeners()
-    this._connection = this._pgClient.connection
-    this._pgClient = null
+    pgClient.connection.removeAllListeners();
+    this._connection = pgClient.connection;
 
-    this._connection.on('errorMessage', this._handleError.bind(this))
-    this._connection.on('error', this._handleError.bind(this))
-    this._connection.on('end', this._handleEnd.bind(this))
-    this._connection.on('rowDescription', this._handleRowDescription.bind(this))
-    this._connection.on('parameterDescription', this._handleParameterDescription.bind(this))
-    this._connection.on('readyForQuery', this._handleReadyForQuery.bind(this))
+    this._connection.on("errorMessage", (err) => {
+      this._listener
+        ? this._listener("errorMessage", err)
+        : console.error("Unexpected error:", err);
+    });
 
-    this._connecting = false;
-    this._connected = true
+    this._connection.on("error", (err) => {
+      this._listener
+        ? this._listener("error", err)
+        : console.error("Unexpected error:", err);
+    });
+
+    this._connection.on("end", () => {
+      this._listener && this._listener("end");
+      this._connection = null;
+    });
+
+    this._connection.on("rowDescription", (msg) => {
+      this._listener
+        ? this._listener("rowDescription", msg)
+        : console.error("Unexpected RowDescription message:", msg);
+    });
+
+    this._connection.on("parameterDescription", (msg) => {
+      this._listener
+        ? this._listener("parameterDescription", msg)
+        : console.error("Unexpected ParameterDescription message:", msg);
+    });
+
+    this._connection.on("readyForQuery", () => {
+      this._listener
+        ? this._listener("readyForQuery")
+        : console.error("Unexpected ReadyForQuery message:", msg);
+    });
   }
+
   terminate() {
-    if (this._terminated) {
-      console.warn('Already terminated')
+    if (this._connection === null) {
+      console.warn("Already terminated");
     } else {
-      this._terminated = true
-      const target = this._pgClient ?? this._connection
-      this._pgClient = null
-      this._connection = null
-      target.end()
+      const connection = this._connection;
+      this._connection = null;
+      connection.end();
     }
   }
+
   async describe(query) {
-    if (this._terminated) {
-      return Promise.reject(new Error('The client has been terminated'))
+    if (this._connection === null) {
+      return Promise.reject(new Error("The client has been terminated"));
     }
 
-    if (this._currentRequestCb !== null) {
-      return Promise.reject(new Error('Another request is in progress'))
+    if (this._listener !== null) {
+      return Promise.reject(new Error("Another request is in progress"));
     }
 
-    if (!this._connected) {
-      await this.connect()
-    }
+    return new Promise((resolve, reject) => {
+      let rowDescription;
+      let parameterDescription;
 
-    let resolve;
-    let reject;
-    const promise = new Promise((resolve_, reject_) => { resolve = resolve_; reject = reject_ })
-    this._currentRequestCb = (err, data) => {
-      if (err) { reject(err) }
-      else { resolve(prepareData(data, this._typesInfo, this._columnsInfo)) }
-    }
+      this._listener = (tag, arg) => {
+        switch (tag) {
+          case "errorMessage":
+          case "error":
+            reject(arg);
+            this._listener = null;
+            break;
 
-    // https://www.postgresql.org/docs/14/protocol-flow.html#PROTOCOL-FLOW-EXT-QUERY
-    this._connection.parse({ name: '', text: query })
-    this._connection.describe({ name: '', type: 'S' })
-    this._connection.sync()
+          case "end":
+            reject(new Error("Connection has been terminated"));
+            this._listener = null;
+            break;
 
-    return promise
+          case "rowDescription":
+            rowDescription = arg;
+            break;
+
+          case "parameterDescription":
+            parameterDescription = arg;
+            break;
+
+          case "readyForQuery":
+            resolve({
+              input: parameterDescription.dataTypeIDs.map((id) =>
+                this._types.get(id)
+              ),
+              output:
+                rowDescription == null
+                  ? null
+                  : rowDescription.fields.map((field) => {
+                      return {
+                        name: field.name,
+                        type: this._types.get(field.dataTypeID),
+                        collumn: this._columns.get(
+                          [field.tableID, field.columnID].join(",")
+                        ),
+                      };
+                    }),
+            });
+            this._listener = null;
+            break;
+        }
+      };
+
+      // https://www.postgresql.org/docs/14/protocol-flow.html#PROTOCOL-FLOW-EXT-QUERY
+      this._connection.parse({ name: "", text: query });
+      this._connection.describe({ name: "", type: "S" });
+      this._connection.sync();
+    });
   }
 }
 
-exports.Client = DescribeClient
+exports.createClient = async (options) => {
+  const client = new DescribeClient(options);
+  await client._connect();
+  return client;
+};
--- packages/playground/src/test.js
+++ packages/playground/src/test.js
@@ -1,20 +1,24 @@
-const { Client } = require("@typesafe-sql/describe-query")
+const { createClient } = require("@typesafe-sql/describe-query");
 
 async function main() {
-  const client = new Client({
-    host: 'localhost',
+  const client = await createClient({
+    host: "localhost",
     port: 5432,
-    user: 'roman',
-    password: '1234',
-    database: 'roman',
-  })
+    user: "roman",
+    password: "1234",
+    database: "roman",
+  });
 
-  console.log(await client.describe('select name as nickname, * from people'));
-  console.log(await client.describe('insert into people values ($1, $2, $3)'));
-  console.log(await client.describe('SELECT $1::text as test, $2::int as test1, * from people'))
-  console.log(await client.describe('SELECT NOW();'))
+  console.log(await client.describe("select name as nickname, * from people"));
+  console.log(await client.describe("insert into people values ($1, $2, $3)"));
+  console.log(
+    await client.describe(
+      "SELECT $1::text as test, $2::int as test1, * from people"
+    )
+  );
+  console.log(await client.describe("SELECT NOW();"));
 
-  client.terminate()
+  client.terminate();
 }
 
-main()
+main();
