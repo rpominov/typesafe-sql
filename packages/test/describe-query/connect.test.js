95ed66c1ac0e9f901465e2ca7f6bc9df9bb46136
Roman <rpominov@gmail.com>
2022-01-28 18:28:08 +0300

    changes

--- packages/test/describe-query/connect.test.js
+++ packages/test/describe-query/connect.test.js
@@ -1,18 +1,10 @@
 const { createClient } = require("@typesafe-sql/describe-query");
 const { Client } = require("pg");
 
-let pgClient;
-
-beforeAll(async () => {
-  pgClient = new Client();
-  await pgClient.connect();
-});
-
-afterAll(() => {
-  pgClient.end();
-});
-
 test("Connects and disconnects", async () => {
+  const pgClient = new Client();
+  await pgClient.connect();
+
   const isConnected = async () => {
     let { rows } = await pgClient.query(
       "SELECT count(*) FROM pg_stat_activity WHERE application_name = 'test_app'"
@@ -30,13 +22,45 @@ test("Connects and disconnects", async () => {
 
   expect(await isConnected()).toBe(false);
 
+  pgClient.end();
+});
+
+test("Fatal error propagates to all requests in the queue", async () => {
+  const client = await createClient({ application_name: "test_app" });
+
+  const a = client.describe("SELECT 1");
+  const b = client.describe("SELECT 1");
+  const c = client.describe("SELECT 1");
+
+  client.terminate();
+
+  try {
+    await a;
+  } catch (e) {}
+  try {
+    await b;
+  } catch (e) {}
+  let error;
+  try {
+    await c;
+  } catch (e) {
+    error = e;
+  }
+  expect(error).toMatchInlineSnapshot(
+    `[Error: Connection has been terminated]`
+  );
+});
+
+test("Requests fail after termination", async () => {
+  const client = await createClient({ application_name: "test_app" });
+  client.terminate();
+
   let error;
   try {
     await client.describe("SELECT 1");
   } catch (e) {
     error = e;
   }
-
   expect(error).toMatchInlineSnapshot(
     `[Error: Connection has been terminated]`
   );
