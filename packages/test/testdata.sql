8cacc4351086ebb86cc2022c03f83fb9e0465b9a
Roman <rpominov@gmail.com>
2022-01-28 13:52:34 +0300

    minor

--- packages/test/testdata.sql
+++ packages/test/testdata.sql
@@ -1,8 +1,7 @@
 
-DROP TABLe IF EXISTS animals;
+DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name text,
     is_dog boolean NOT NULL DEFAULT true
 );
--- CREATE UNIQUE INDEX animals_pkey ON animals(id int4_ops);
